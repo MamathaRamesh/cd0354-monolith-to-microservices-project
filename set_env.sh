7a2ad0e130ddf72a9da27029600880480f555eb7
Mamatha Ramesha <your-github-email@example.com>
2026-09-11 11:08:13 +0000

    Add microservices, Docker setup, and GitHub Actions CI/CD

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=Udg2026!SecureDB
+export POSTGRES_HOST=database-1.cbgg4d27bnax.us-west-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=mybucket1200798
-export AWS_REGION=us-east-1
+export AWS_BUCKET=mamatha-udagram-images
+export AWS_REGION=us-west-2
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=udagram-secret-key-2026
 export URL=http://localhost:8100
