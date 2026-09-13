30b5c2857033b453fd7c0386d3889b01899d3af6
Mamatha Ramesha <your-github-email@example.com>
2026-09-13 07:02:28 +0000

    Fix replicas and add screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1d47d61a25e04e46b21e993c7c36a3d-1896621624.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
